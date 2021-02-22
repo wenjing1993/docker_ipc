809217adec889011dce43930668838dd64a7f65e
wenjing1993 <784142461@qq.com>
2021-02-22 16:11:52 +0800

    加入注释

--- docker_ipc_test/share1.cpp
+++ docker_ipc_test/share1.cpp
@@ -14,7 +14,7 @@ void signal_handle(int sig_no, siginfo_t * sig_info, void *context) {
 }
 
 int main() {
-
+    //设置中断函数处理
     struct sigaction act;
     act.sa_flags = SA_SIGINFO;
     act.sa_sigaction = signal_handle;
@@ -24,13 +24,16 @@ int main() {
     sigaction(SIGQUIT, &act, NULL);
     sigaction(SIGSEGV, &act, NULL);
     
+    //创建共享内存
     shm_id = create_shm(BASE_ID, SHM_SIZE);
     printf("create shm_id: %d\n", shm_id);
 
+    //写入数据
     int* write_val = (int*) shmat(shm_id, NULL, 0);
     *write_val = 11111;
     printf("share1 is writing: %d\n", *write_val);
 
+    //挂起
     usleep(1 * 60 * 60 * 1000 * 1000);  //sleep 1 hour
 
     return 0;
--- docker_ipc_test/share2.cpp
+++ docker_ipc_test/share2.cpp
@@ -5,6 +5,7 @@
 #include "public.hpp"
 
 int main() {
+    //获取共享内存id
     int shm_id = get_shm(BASE_ID);
     printf("get shm_id: %d\n", shm_id);
     if (shm_id == -1) {
@@ -12,12 +13,14 @@ int main() {
         return -1;
     }
 
+    //连接共享内存
     int* read_val = (int*) shmat(shm_id, NULL, 0);
     
     printf("share2 is reading: %d\n", *read_val);
     
     // usleep(1 * 60 * 1000 * 1000);  //sleep 1 min
 
+    //从共享内存中分离
     shmdt(read_val);
 
     return 0;
